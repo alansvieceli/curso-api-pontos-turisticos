f052cafd1a44d1b6bb4d0d5f22cf877915d9b160
Alan Vieceli <alansvieceli@gmail.com>
2018-11-20 20:45:49 -0200

    Create app comentarios

--- core/models.py
+++ core/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from atracoes.models import Atracao
+from comentarios.models import Comentario
 
 class PontoTurstico(models.Model):
     nome = models.CharField(max_length=150)
     descricao = models.TextField()
     aprovado = models.BooleanField(default=False)
     atracoes = models.ManyToManyField(Atracao)
-    #comentarios = models.ManyToManyField(Comentario)
+    comentarios = models.ManyToManyField(Comentario)
     #avaliacoes = models.ManyToManyField(Avaliacao)
     #endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE, null=True, blank=True)
 
